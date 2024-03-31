2a3b12cb46570d4cc035de8a93d56eefb5b314e2
Anang Novriadi <anangnov99@gmail.com>
2024-03-31 14:13:56 +0700

    update improve

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = ({ title }: HeaderProps) => {
       <nav className="bg-white border-gray-200 dark:bg-gray-900" style={{ backgroundColor: '#CAEEFB' }}>
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="/logo_goarif_black.png" className="h-10" alt="Logo" />
+            <img src="/logo_goarif_black.png" className="h-16" alt="Logo" />
           </a>
           <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
@@ -15,16 +15,13 @@ const Header = ({ title }: HeaderProps) => {
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 items-center md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
               <li>
-              <a href="/" className="block py-2 px-3 text-gray-900 rounded md:hover:text-blue-700 font-bold font-mono">Home</a>
+              <a href="/" className="block py-2 px-3 text-gray-900 rounded md:hover:text-blue-700 font-bold ">Home</a>
               </li>
               <li>
-                <a href="/" className="block py-2 px-3 text-gray-900 rounded md:hover:text-blue-700 font-bold font-mono">About</a>
+                <a href="/" className="block py-2 px-3 text-gray-900 rounded md:hover:text-blue-700 font-bold ">About</a>
               </li>
               <li>
-                <a href="/" className="block py-2 px-3 text-gray-900 rounded md:hover:text-blue-700 font-bold font-mono">Contact</a>
-              </li>
-              <li>
-                <a type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 font-mono">Join our waitlist</a>
+                <a href="https://wa.me/628117576088" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">Get Demo</a>
               </li>
             </ul>
           </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,41 +2,84 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import Card from '../components/Card';
 import { TypeAnimation } from 'react-type-animation';
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
 
 const Home: React.FC = () => {
+  const [open, setOpen] = React.useState(1);
+ 
+  const handleOpen = (value: any) => setOpen(open === value ? 0 : value);
+
   return (
     <>
       <div className="mx-auto px-24 py-10" style={{ backgroundColor: '#CAEEFB' }}>
         <div className="mx-auto flex justify-between gap-10">
           <div>
             <h1 className="text-white text-4xl font-bold mb-5"></h1>
-            <h1 className="text-2xl text-gray-800 mb-8 font-mono">
-              Welcome to Arif - Where Data Meets Intelligence!
+            <h1 className="text-2xl text-gray-800 mb-8 ">
+              Welcome to Arif. Where Data Meets Intelligence!
             </h1>
-            <a href="https://wa.me/628117576088" className="shadow focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-800 dark:focus:ring-green-800 font-mono">Start Try</a>
+            {/* <a href="https://wa.me/628117576088" className="shadow focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-800 dark:focus:ring-green-800 ">Let's connect</a> */}
           </div>
-          <div className="w-full">
-            <img src="/assets/banner.png" className="" alt="Banner" />
+          <div className="w-auto">
+            <img src="/assets/banner1.png" className="h-[400px]" alt="Banner" />
           </div>
         </div>
       </div>
       <div className="mx-auto px-24 py-24">
         <div className="text-center px-[140px]">
-          <p className="text-3xl text-gray-600 font-mono">Arif allows you to interact directly with your data, asking questions and receiving instant, contextual answers. Whether you're identifying consumer insights or evaluating operational efficiencies, your data is no longer a passive report, it will take you to actionable insights.</p>
+          <p className="text-3xl text-gray-600 ">Arif allows you to interact directly with your data, asking questions and receiving instant, contextual answers. Whether you're identifying consumer insights or evaluating operational efficiencies, your data is no longer a passive report, it will take you to actionable insights.</p>
         </div>
       </div>
       <div className="mx-auto px-24 pt-16 pb-10">
         <div className="flex justify-between gap-10">
           <div className="w-full">
-            <img src="/assets/content1.png" className="" alt="Banner" />
+            <img src="/assets/banner1.png" className="" alt="Banner" />
           </div>
           <div className="w-5/6">
-            <p className="text-lg font-bold text-gray-700 mb-5 font-mono">Key Features:</p>
-            <p className="text-md text-gray-500 mb-2 font-mono"><b>Intelligent Data Visualization: </b>Arif platform offers intuitive and dynamic data visualization tools that transform complex datasets into actionable insights. From interactive charts to customizable dashboards, visualize key metrics with ease and clarity.</p>
-            <p className="text-md text-gray-500 mb-2 font-mono"><b>Rapid Audio and Video Transcription: </b>Say goodbye to time-consuming transcription tasks. Transcribing audio and video files is effortless and instantaneous. Arif AI-powered transcription engine accurately converts spoken words into text within seconds, saving you valuable time and resources.</p>
-            <p className="text-md text-gray-500 mb-2 font-mono"><b>AI-Powered Insights: </b>Leverage the power of artificial intelligence to uncover hidden patterns, trends, and correlations within your data. Our advanced analytics algorithms analyze vast amounts of data to provide you with actionable insights that drive business growth and innovation.</p>
-            <p className="text-md text-gray-500 mb-2 font-mono"><b>Scalable and Secure: </b>Whether you're a small startup or a large enterprise, Arif scales to meet your needs. Rest assured knowing that your data is protected with state-of-the-art security measures, ensuring confidentiality and compliance at every step.</p>
-            <p className="text-md text-gray-500 mb-2 font-mono"><b>Integration: </b>Our platform is designed to work with a wide range of data sources and formats, allowing you to maximize efficiency and productivity across your organization.</p>
+            <>
+              <Accordion open={open === 1} placeholder={undefined}>
+                <AccordionHeader onClick={() => handleOpen(1)} placeholder={undefined}>Intelligent Data Visualization</AccordionHeader>
+                <AccordionBody>
+                  Arif makes complex data easy to understand through user-friendly dashboards and charts.
+                </AccordionBody>
+              </Accordion>
+              <Accordion open={open === 2} placeholder={undefined}>
+                <AccordionHeader onClick={() => handleOpen(2)} placeholder={undefined}>
+                  Rapid Audio and Video Transcription
+                </AccordionHeader>
+                <AccordionBody>
+                  Quickly converts audio and video into accurate text transcripts with Arif platform.
+                </AccordionBody>
+              </Accordion>
+              <Accordion open={open === 3} placeholder={undefined}>
+                <AccordionHeader onClick={() => handleOpen(2)} placeholder={undefined}>
+                  AI-Powered Insights
+                </AccordionHeader>
+                <AccordionBody>
+                  Arif uses AI to find meaningful patterns in large amounts of data, giving you the knowledge to make better decisions.
+                </AccordionBody>
+              </Accordion>
+              <Accordion open={open === 4} placeholder={undefined}>
+                <AccordionHeader onClick={() => handleOpen(2)} placeholder={undefined}>
+                  Scalable and Secure
+                </AccordionHeader>
+                <AccordionBody>
+                  Our Arif platform work for businesses of all sizes and prioritizes the security of your data.
+                </AccordionBody>
+              </Accordion>
+              <Accordion open={open === 5} placeholder={undefined}>
+                <AccordionHeader onClick={() => handleOpen(2)} placeholder={undefined}>
+                  Integration
+                </AccordionHeader>
+                <AccordionBody>
+                  Arif connects with the various data sources you already use, streamlining your workflow.
+                </AccordionBody>
+              </Accordion>
+            </>
           </div>
         </div>
       </div>
@@ -44,12 +87,12 @@ const Home: React.FC = () => {
       <div className="mx-auto mt-16 px-24 pt-16 pb-16" style={{ backgroundColor: '#FAF9F5' }}>
         <div className="text-center">
           <div className="mb-8">
-            <p className="text-xl text-gray-600 font-bold font-mono">Trusted by.</p>
+            {/* <p className="text-xl text-gray-600 font-bold ">Trusted by.</p> */}
           </div>
           <div className="mx-auto flex justify-center gap-10">
-            <img src="/assets/antler.png" className="" alt="Banner" />
-            <img src="/assets/antler.png" className="" alt="Banner" />
-            <img src="/assets/antler.png" className="" alt="Banner" />
+            {/* <img src="/assets/antler.png" className="" alt="Banner" /> */}
+            {/* <img src="/assets/antler.png" className="" alt="Banner" /> */}
+            {/* <img src="/assets/antler.png" className="" alt="Banner" /> */}
           </div>
         </div>
       </div>
@@ -58,33 +101,33 @@ const Home: React.FC = () => {
         <form className="mx-auto">
           <div className="mx-auto grid grid-cols-2 gap-4 mb-2">
             <div className="mb-5">
-              <label className="block mb-2 text-sm font-medium text-gray-900 font-mono">First Name</label>
+              <label className="block mb-2 text-sm font-medium text-gray-900 ">First Name</label>
               <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="" placeholder="First Name" />
             </div>
             <div className="mb-5">
-              <label className="block mb-2 text-sm font-medium text-gray-900 font-mono">Last Name</label>
+              <label className="block mb-2 text-sm font-medium text-gray-900 ">Last Name</label>
               <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="" placeholder="Last Name" />
             </div>
           </div>
           <div className="mx-auto grid grid-cols-2 gap-4 mb-2">
             <div className="mb-5">
-              <label className="block mb-2 text-sm font-medium text-gray-900 font-mono">Company</label>
+              <label className="block mb-2 text-sm font-medium text-gray-900 ">Company</label>
               <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="" placeholder="Company" />
             </div>
             <div className="mb-5">
-              <label className="block mb-2 text-sm font-medium text-gray-900 font-mono">Email</label>
+              <label className="block mb-2 text-sm font-medium text-gray-900 ">Email</label>
               <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="" placeholder="Email" />
             </div>
           </div>
           <div className="mx-auto mb-2">
             <div className="mb-5">
-              <label className="block mb-2 text-sm font-medium text-gray-900 font-mono">Organization</label>
+              <label className="block mb-2 text-sm font-medium text-gray-900 ">Organization</label>
               <textarea rows={3} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" value="" placeholder="Tell us a little about your organization and needs"></textarea>
             </div>
           </div>
           <div className="mx-auto mb-2">
             <div className="mx-auto text-center mb-5">
-              <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 font-mono">Submit</button>
+              <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">Submit</button>
             </div>
           </div>
         </form>
@@ -99,15 +142,15 @@ const Home: React.FC = () => {
             </a>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
               <li>
-                <a href="/" className="hover:underline me-4 md:me-6 font-mono">Privacy Policy</a>
+                <a href="/" className="hover:underline me-4 md:me-6 ">Privacy Policy</a>
               </li>
               <li>
-                <a href="/" className="hover:underline font-mono">Term of Use</a>
+                <a href="/" className="hover:underline ">Term of Use</a>
               </li>
             </ul>
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400 font-mono">© 2024 <a href="https://flowbite.com/" className="hover:underline">GoArif</a>. All Rights Reserved.</span>
+          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400 ">© 2024 <a href="https://flowbite.com/" className="hover:underline">GoArif</a>. All Rights Reserved.</span>
         </div>
       </footer>
 
